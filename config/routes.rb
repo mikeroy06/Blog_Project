cf62ac5e70e643b7c472f7931a1342b449a1fa19
Mike Roy <mikeroy06@bellsouth.net>
2017-09-18 12:31:19 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :comments
+  resources :posts
+  get 'index' => 'practice#index'
+  post 'index' => 'practice#index'
+
+  get 'about' => 'practice#about'
+
+  root 'posts#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+
